6b018125a56ebd547edfecb45ded91f2338df707
Savagecoder77 <savagecoder77@gmail.com>
2020-02-19 22:47:06 -0800

    new initializiation file

---
+++ jyl/__init__.py
@@ -0,0 +1,12 @@
+# Flask imports
+from flask import Flask
+from config import Config
+
+# Create Flask app
+app = Flask(__name__)
+
+# Add Configurations to app
+app.config.from_object(Config)
+
+# Import all views
+import jyl.views
