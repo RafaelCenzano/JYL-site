f75bac0d52c299270fa7c8eba55079bb89df3957
Savagecoder77 <savagecoder77@gmail.com>
2020-02-19 22:48:00 -0800

    make correction to run file

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from jyl import app
 # Run Flask app
 app.run(
     host='0.0.0.0', # host to view from outside the network
-    port=8080,  # assign to port 8080
-    debug=True # Have debug pages show when there is an error
+    port=8080 # assign to port 8080
 )
