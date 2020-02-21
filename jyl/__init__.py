8ee8d93a848572e4a425b1eb310fbe9012cc5f15
Savagecoder77 <savagecoder77@gmail.com>
2020-02-21 00:15:50 -0800

    new things to initialize

--- jyl/__init__.py
+++ jyl/__init__.py
@@ -1,12 +1,32 @@
-# Flask imports
+# Import Flask for flask app object
 from flask import Flask
+
+# Import Flask modules to create objects for our app
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-# Create Flask app
+# Create flask app object
 app = Flask(__name__)
 
-# Add Configurations to app
+# Add configurations to app
 app.config.from_object(Config)
+
+# Create Database object from flask app object
+db = SQLAlchemy(app)
+
+# Create Login manager
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+# Create mail object from flash app object
+#mail = Mail(app)
+
+# importing all the models and initializing them
+from app.models import *
+db.create_all()
 
 # Import all views
 import jyl.views
