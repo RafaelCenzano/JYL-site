a8eb0ef4b587762cd64f7c976635ffa494465a69
Savagecoder77 <savagecoder77@gmail.com>
2020-02-21 00:02:47 -0800

    add user

--- jyl/views.py
+++ jyl/views.py
@@ -1,6 +1,6 @@
 from jyl import app, forms
 from flask import render_template, redirect, url_for, request, flash, make_response
-from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import login_user, current_user, logout_user, login_required, current_user
 from app.forms import LoginForm, RequestResetForm, ResetPasswordForm
 
 
