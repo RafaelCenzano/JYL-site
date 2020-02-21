38fbac233f1d0cc3f4d4d09c9b0aeceadbb95006
Savagecoder77 <savagecoder77@gmail.com>
2020-02-21 00:09:44 -0800

    redo config file

--- config.py
+++ config.py
@@ -1,12 +1,30 @@
+from jyl import secret
 import os
 
 '''
-Config file containing Config class
+To create a secret key run the program
+import secrets
+secrets.token_hex(16)
 '''
 
+'''
+Config class
+'''
+class Config(object):
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
+    SECRET_KEY = os.environ.get('jylKey')
+    SECURITY_PASSWORD_SALT = os.environ.get('jylSalt')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
-    DEBUG = False
-    TESTING = False
-    SECRET_KEY = os.urandom(32)
+    '''
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'email@gmail.com'
+    MAIL_PASSWORD = secret.EMAIL_PASS
+    MAIL_DEFAULT_SENDER = 'JYL Toolbox <email@gmail.com>'
+    '''
