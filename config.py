62c25de7fa42365aae405d3d7da9cd4667533e9a
Savagecoder77 <savagecoder77@gmail.com>
2020-02-22 11:41:33 -0800

    change config

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('jylKey')
     SECURITY_PASSWORD_SALT = os.environ.get('jylSalt')
+
+    TESTING = True
+    DEBUG = True
+
+    # Using Postgresql
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
