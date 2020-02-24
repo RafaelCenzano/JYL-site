e64e038def173f2432c944fe9f911f1fd146c234
Savagecoder77 <savagecoder77@gmail.com>
2020-02-23 22:12:23 -0800

    new changes

--- jyl/models.py
+++ jyl/models.py
@@ -22,6 +22,7 @@ class User(db.Model, UserMixin):
         nullable=False,
         default=datetime.utcnow)
     admin = db.Column(db.Boolean, unique=False, default=False)
+    leader = db.Column(db.Boolean, unique=False, default=False)
 
     def __repr__(self):
         return f'User({self.username}, {self.email})'
