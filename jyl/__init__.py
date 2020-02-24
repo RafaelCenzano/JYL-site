683d5e69c1f42b6dfedbeabe12b1aca73f46a206
Savagecoder77 <savagecoder77@gmail.com>
2020-02-24 13:29:43 -0800

    testing

--- jyl/__init__.py
+++ jyl/__init__.py
@@ -29,14 +29,11 @@ login_manager.login_message_category = 'info'
 # importing all the models and initializing them
 from jyl.models import *
 db.create_all()
-'''
-user_1 = User(username='raf', email='r@demo.com', password='pass')
+
+user_1 = User(firstname='rafael', lastname='cenzano', email='r@demo.com', password='pass', confirmed=True, hours=0, nickname='raf', nicknameapprove=False)
+db.session.add(user_1)
+user_2 = User(firstname='rafael', lastname='cenzano', email='ra@demo.com', password='pass', confirmed=True, hours=0, nickname='raf', nicknameapprove=True)
 db.session.add(user_2)
-user_2 = User(username='raf', email='r@demo.com', password='pass')
-db.session.add(user_2)
-user_2 = User(username='raf', email='r@demo.com', password='pass')
-db.session.add(user_2)
-'''
 
 # Import all views
 import jyl.views
