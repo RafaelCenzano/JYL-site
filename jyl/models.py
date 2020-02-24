296e820fc50da42a1680c201d1cbf984b66acfda
Savagecoder77 <savagecoder77@gmail.com>
2020-02-24 00:05:54 -0800

    correct spelling

--- jyl/models.py
+++ jyl/models.py
@@ -23,7 +23,7 @@ class User(db.Model, UserMixin):
         default=datetime.utcnow)
     admin = db.Column(db.Boolean, unique=False, default=False)
     leader = db.Column(db.Boolean, unique=False, default=False)
-    hours = db.Column(db.Float, unqiue=False)
+    hours = db.Column(db.Float, unique=False)
     nicknameapprove = db.Column(db.Boolean, unique=False, default=False)
 
     def __repr__(self):
