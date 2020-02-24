63174b89e33e73db0d17f4f524ec111b06cbbe8b
Savagecoder77 <savagecoder77@gmail.com>
2020-02-24 13:44:03 -0800

    fix imports

--- initializedb.py
+++ initializedb.py
@@ -1,6 +1,7 @@
 from jyl import db
+from jyl import models
 
-user_1 = User(firstname='rafael', lastname='cenzano', email='r@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=False)
+user_1 = models.User(firstname='rafael', lastname='cenzano', email='r@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=False)
 db.session.add(user_1)
-user_2 = User(firstname='rafael', lastname='cenzano', email='ra@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=True)
+user_2 = models.User(firstname='rafael', lastname='cenzano', email='ra@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=True)
 db.session.add(user_2)
