edaf536b6ba208b6b2963749c5c1beae1a1437d5
Savagecoder77 <savagecoder77@gmail.com>
2020-02-24 11:25:58 -0800

    redo cookie system

--- jyl/views.py
+++ jyl/views.py
@@ -12,8 +12,12 @@ Views
 @app.route('/home/', methods=['GET'])
 def index():
 
+    if 'current' in request.cookies:
+        current = request.cookies['current']
+        page.set_cookie('page', current, max_age=60 * 60 * 24 * 365)
+
     page = make_response(render_template('home.html'))
-    page.set_cookie('page', 'index', max_age=60 * 60 * 24 * 365)
+    page.set_cookie('current', 'index', max_age=60 * 60 * 24 * 365)
     return page
 
 
@@ -50,8 +54,12 @@ def login():
                 f'Login Unsuccessful. Please check email and password{confirmed}',
                 'error')
 
+    if 'current' in request.cookies:
+        current = request.cookies['current']
+        page.set_cookie('page', current, max_age=60 * 60 * 24 * 365)
+
     page = make_response(render_template('login.html', form=form))
-    page.set_cookie('page', 'login', max_age=60 * 60 * 24 * 365)
+    page.set_cookie('current', 'login', max_age=60 * 60 * 24 * 365)
     return page
 
 
@@ -157,8 +165,12 @@ def logout():
 @app.route('/license', methods=['GET'])
 def license():
 
+    if 'current' in request.cookies:
+        current = request.cookies['current']
+        page.set_cookie('page', current, max_age=60 * 60 * 24 * 365)
+
     page = make_response(render_template('license.html'))
-    page.set_cookie('page', 'license', max_age=60 * 60 * 24 * 365)
+    page.set_cookie('current', 'license', max_age=60 * 60 * 24 * 365)
     return page
 
 
