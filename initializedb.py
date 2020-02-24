3cb46f36bda11884917075b0b19e2a2bcf29338e
Savagecoder77 <savagecoder77@gmail.com>
2020-02-24 14:02:36 -0800

    update initializtion file

--- initializedb.py
+++ initializedb.py
@@ -1,7 +1,13 @@
 from jyl import db
-from jyl import models
+from jyl.models import User
 
-user_1 = models.User(firstname='rafael', lastname='cenzano', email='r@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=False)
+db.create_all()
+
+user_1 = User(firstname='rafael', lastname='cenzano', email='r@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=False)
 db.session.add(user_1)
-user_2 = models.User(firstname='rafael', lastname='cenzano', email='ra@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=True)
+user_2 = User(firstname='rafael', lastname='cenzano', email='ra@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=True)
 db.session.add(user_2)
+
+# commit the chagnes to the db
+db.session.commit()
+
