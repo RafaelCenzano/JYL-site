73c018b26aa37822d0214f1093ed9a4dcad53a6b
Savagecoder77 <savagecoder77@gmail.com>
2020-02-24 16:32:01 -0800

    add proper hashing

--- initializedb.py
+++ initializedb.py
@@ -1,11 +1,22 @@
-from jyl import db
+from jyl import db, app, bcrypt
 from jyl.models import User
+from hashlib import sha256
 
 db.create_all()
 
-user_1 = User(firstname='rafael', lastname='cenzano', email='r@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=False)
+pass1 = bcrypt.generate_password_hash(
+            sha256(
+                ('pass' +
+                 'raf@demo.com' +
+                 app.config['SECURITY_PASSWORD_SALT']).encode('utf-8')).hexdigest()).decode('utf-8')
+pass2 = bcrypt.generate_password_hash(
+            sha256(
+                ('pass' +
+                 'rafa@demo.com' +
+                 app.config['SECURITY_PASSWORD_SALT']).encode('utf-8')).hexdigest()).decode('utf-8')
+user_1 = User(firstname='rafael', lastname='cenzano', email='raf@demo.com', password=pass1, confirmed=True, hours=0.0, nickname='raf', nicknameapprove=False)
 db.session.add(user_1)
-user_2 = User(firstname='rafael', lastname='cenzano', email='ra@demo.com', password='pass', confirmed=True, hours=0.0, nickname='raf', nicknameapprove=True)
+user_2 = User(firstname='rafael', lastname='cenzano', email='rafa@demo.com', password=pass2, confirmed=True, hours=0.0, nickname='raf', nicknameapprove=True)
 db.session.add(user_2)
 
 # commit the chagnes to the db
