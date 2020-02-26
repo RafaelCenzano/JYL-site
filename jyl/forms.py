7e8f9e6e6543046984a2a68a89e92589082aae3d
Savagecoder77 <savagecoder77@gmail.com>
2020-02-25 16:26:44 -0800

    clean up file

--- jyl/forms.py
+++ jyl/forms.py
@@ -5,17 +5,8 @@ from jyl.models import User
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
-def model_exists(model_class):
-    try:
-        model_class.query.first()
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-
 '''
-Create a registation form
+Forms for jyl toolbox
 '''
 
 
@@ -31,13 +22,6 @@ class RequestResetForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
-
-    def validate_email(self, email):
-        if model_exists(User):
-            user = User.query.filter_by(email=email.data).first()
-            if user is None:
-                raise ValidationError(
-                    'There is no account with that email. You must register first.')
 
 
 class ResetPasswordForm(FlaskForm):
