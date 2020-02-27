141523b0eecf8e2af8972459f207f7d7c85fb52c
Savagecoder77 <savagecoder77@gmail.com>
2020-02-26 22:02:50 -0800

    fix

--- initializedb.py
+++ initializedb.py
@@ -5,12 +5,7 @@ from datetime import datetime
 
 db.create_all()
 
-user2 = User.query.filter_by(email='rafa@demo.com').first()
-user2.namecount = 1
 
-db.session.commit()
-
-'''
 pass1 = bcrypt.generate_password_hash(
             sha256(
                 ('pass' +
@@ -68,4 +63,4 @@ db.session.add(usermeeting3)
 
 # commit the chagnes to the db
 db.session.commit()
-'''
+
