854ef264368ce1f8321b8dbb87fe3a04c15f23d5
Savagecoder77 <savagecoder77@gmail.com>
2020-02-27 17:52:55 -0800

    fixes

--- jyl/helpers.py
+++ jyl/helpers.py
@@ -14,5 +14,13 @@ def load_user(id):
 def sendoff(where):
     if 'current' in request.cookies:
         page = request.cookies['current']
+        if 'profile' in page:
+            num = request.cookies['profile-num']
+            first = request.cookies['profile-first']
+            last = request.cookies['profile-last']
+            return redirect(url_for('profile', num=num, first=first, last=last))
+        elif 'meeting' in page:
+            meetingid = request.cookies['meeting-id']
+            return redirect(url_for('meeting', idOfMeeting=meetingid))
         return redirect(url_for(page))
     return redirect(url_for(where))
