186d1909b46b9bd8be7d0ed5fd6c9564e34140ae
Savagecoder77 <savagecoder77@gmail.com>
2020-03-06 15:43:50 -0800

    changes color based off number of hours

--- jyl/static/js/profile.js
+++ jyl/static/js/profile.js
@@ -1,20 +1,19 @@
 var values = document.getElementsByClassName('hours');
-var number = parseInt(values[0].value);
+var number = parseInt(values[0].textContent);
 var redNum = 0;
 var greenNum = 0;
-if (number >= 85) {
+var blueNum = 0;
+if(number >= 85){
   greenNum = 255;
-} else if (number > (42.5)) {
+}else if(number < 42.5){
+    greenNum = 0;
+  redNum = 255;
+  greenNum += 6 * number;
+}else{
+    redNum = 255;
   greenNum = 255;
-  redNum = 255;
-  var multiplyer = number - 42.5;
-  redNum -= 6 * multiplyer;
-} else {
-  greenNum = 0;
-  redNum = 255;
-  var multiplyer = number - 42.5;
-  redNum += 6 * multiplyer;
+  redNum -= 6 * (number - 42.5);
 }
 for (var i = 0; i < values.length; i++) {
-  values[i].style.backgroundColor = "rgb(" + redNum + ", " + greenNum + ", 60)";
+  values[i].style.backgroundColor = "rgb(" + redNum + ", " + greenNum + ", 0)";
 }
