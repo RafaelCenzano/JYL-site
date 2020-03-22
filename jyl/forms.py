2ef73de012eb70a5d5f95a0bb885b880157c4624
Savagecoder77 <savagecoder77@gmail.com>
2020-03-22 12:30:51 -0700

    new create user form and new length validators

--- jyl/forms.py
+++ jyl/forms.py
@@ -2,7 +2,7 @@ from jyl import db
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from flask_wtf import FlaskForm
 from jyl.models import User
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 
 
 '''
@@ -20,14 +20,14 @@ class LoginForm(FlaskForm):
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                        validators=[DataRequired(), Email(), Length(max=120)])
     submit = SubmitField('Request Password Reset')
 
 
 class ResetPasswordForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=10, max=100)])
     confirm_password = PasswordField(
         'Confirm Password', validators=[
             DataRequired(), EqualTo('password')])
@@ -37,12 +37,20 @@ class ResetPasswordForm(FlaskForm):
 class BugReportForm(FlaskForm):
     name = StringField('Name')
     email = StringField('Email')
-    bug = StringField('Bug', validators=[DataRequired()])
+    bug = StringField('Bug', validators=[DataRequired(), Length(max=500)])
     submit = SubmitField('Submit Bug Report')
 
 
 class FeatureRequestForm(FlaskForm):
     name = StringField('Name')
     email = StringField('Email')
-    bug = StringField('Feature', validators=[DataRequired()])
+    bug = StringField('Feature', validators=[DataRequired(), Length(max=500)])
     submit = SubmitField('Submit Feature Request')
+
+
+class CreateUser(FlaskForm):
+    first = StringField('First name', validators=[DataRequired(), Length(max=30)])
+    last = StringField('Last name', validators=[DataRequired(), Length(max=30)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email(), Length(max=120)])
+    submit = SubmitField('Create User')
