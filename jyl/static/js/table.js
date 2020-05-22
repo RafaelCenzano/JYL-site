a17f8c4c21aa6d485e1a0ab8e56ce5cdb95ec01e
Savagecoder77 <savagecoder77@gmail.com>
2020-05-21 17:56:50 -0700

    new js

--- jyl/static/js/table.js
+++ jyl/static/js/table.js
@@ -1,13 +1,26 @@
-$(document).ready(function () {
-$('#selectedColumn').DataTable({
-  "aaSorting": [],
-  columnDefs: [{
-  orderable: false,
-  targets: 3
-  },{
-  orderable: false,
-  targets: 5
-  }]
-});
-  $('.dataTables_length').addClass('bs-select');
-});
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#example tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#example').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every( function () {
+                var that = this;
+ 
+                $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                    if ( that.search() !== this.value ) {
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+        }
+    });
+ 
+} );
