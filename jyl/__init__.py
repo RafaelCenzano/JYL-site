c8c46c6e683a42f1226c29e376b59a823b39be13
RafaelCenzano <savagecoder77@gmail.com>
2020-05-26 13:39:30 -0700

    clean up file

--- jyl/__init__.py
+++ jyl/__init__.py
@@ -1,37 +1,39 @@
 # Import Flask for flask app object
 from flask import Flask
 
-# Import Flask modules to create objects for our app
+# Import Flask modules
 from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
+
 # Create flask app object
 app = Flask(__name__)
-
-# Add configurations to app
 app.config.from_object(Config)
 
-# Create Database object from flask app object
+# Create Database object from brcrypt object
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 
 # Create Login manager
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
 
+
 # Create mail object from flask app object
 #mail = Mail(app)
+
 
 # importing all the models and initializing them
 from jyl.models import *
 db.create_all()
 
+
 # Import all views
 import jyl.helpers
 import jyl.views
 import jyl.accountViews
-import jyl.formViews
