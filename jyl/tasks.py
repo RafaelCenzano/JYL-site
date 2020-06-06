231b51db59a57c287a8cd330c71123fd964cad9e
RafaelCenzano <savagecoder77@gmail.com>
2020-06-05 19:32:54 -0700

    new program that can run tasks at intervals while the app runs

---
+++ jyl/tasks.py
@@ -0,0 +1,20 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import atexit
+from datetime import datetime
+
+def print_time():
+  print(datetime.now().strftime('%H:%M:%S'))
+
+# create schedule for printing time
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=print_time,
+    trigger=IntervalTrigger(minutes=1),
+    id='printing_time_job',
+    name='Print time every minute',
+    replace_existing=True)
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
