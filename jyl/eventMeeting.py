a5926145445e1987fb667df96d2a464e1241a0b2
RafaelCenzano <savagecoder77@gmail.com>
2020-06-18 21:55:38 -0700

    update data for reviews and more efficient code

--- jyl/eventMeeting.py
+++ jyl/eventMeeting.py
@@ -1,6 +1,7 @@
 from jyl import db
 from datetime import datetime
 from jyl.models import Meeting, UserMeeting, Event, UserEvent, User
+from flask_login import login_user, current_user
 
 
 def eventMeetingProccessing(check, meeting):
@@ -11,41 +12,33 @@ def eventMeetingProccessing(check, meeting):
 
         eventMeeting['future'] = True
 
-        if meeting:
-            users = UserMeeting.query.filter_by(
-                meetingid=check.id, going=True, attended=False).all()
-            eventMeeting['meeting'] = True
-        else:
-            users = UserEvent.query.filter_by(
-                eventid=check.id, going=True, attended=False).all()
-            eventMeeting['meeting'] = False
-
-        eventMeeting['future'] = True
-
     else:
 
         eventMeeting['future'] = False
 
-        if meeting:
-            users = UserMeeting.query.filter_by(
-                meetingid=check.id, attended=True).all()
-            eventMeeting['meeting'] = True
-        else:
-            users = UserEvent.query.filter_by(
-                eventid=check.id, attended=True).all()
-            eventMeeting['meeting'] = False
-
-        eventMeeting['future'] = False
-
-    length = check.hourcount
+    if meeting:
+        users = UserMeeting.query.filter_by(
+            meetingid=check.id, attended=True).all()
+        eventMeeting['meeting'] = True
+    else:
+        users = UserEvent.query.filter_by(
+            eventid=check.id, attended=True).all()
+        eventMeeting['meeting'] = False
 
     eventMeeting['userreview'] = []
     eventMeeting['userreviewwho'] = []
     eventMeeting['users'] = []
+    eventMeeting['userReview'] = False
+    eventMeeting['userAttended'] = False
 
     for user in users:
 
+        if current_user.id == user.userid:
+            eventMeeting['userAttended'] = True
+
         if user.comment is not None:
+            if user.userid == current_user.id:
+                eventMeeting['userReview'] = True
             eventMeeting['userreview'].append(user)
             theUser = User.query.get(user.userid)
             eventMeeting['userreviewwho'].append(theUser)
