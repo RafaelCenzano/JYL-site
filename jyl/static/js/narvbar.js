b33acf3157763b84fb0a9742855b3112c38525a8
RafaelCenzano <savagecoder77@gmail.com>
2020-06-19 19:17:34 -0700

    remove js

--- jyl/static/js/narvbar.js
+++ jyl/static/js/narvbar.js
@@ -1,10 +1,3 @@
-document.querySelector("textarea").addEventListener("input", () => {
-    let limit = parseInt(document.querySelector("#wordCount").textContent.split("/")[1]);
-    let len = document.querySelector("textarea").textLength;
-    document.querySelector("textarea").value = document.querySelector("textarea").value.substring(0, Math.min(limit, len));
-    document.querySelector("#wordCount").innerHTML = `${Math.min(len, limit)}/${limit}`;
-});
-​
 document.querySelector("#mobileMenu").addEventListener("click", () => {
     document.querySelector(".sidebar").style.animationDuration = "1.5s";
     document.querySelector("#mobileMenu").style.animationDuration = "1.5s";
