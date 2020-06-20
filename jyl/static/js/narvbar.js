c0f2d1777ba8dd8830955beb7f5996b2a4a72613
RafaelCenzano <savagecoder77@gmail.com>
2020-06-19 19:56:00 -0700

    correct mistake

--- jyl/static/js/narvbar.js
+++ jyl/static/js/narvbar.js
@@ -1,4 +1,4 @@
 document.querySelector("#mobileMenu").addEventListener("click", () => {
     document.querySelector(".sidebar").style.animationDuration = "1.5s";
     document.querySelector("#mobileMenu").style.animationDuration = "1.5s";
-}
+});
