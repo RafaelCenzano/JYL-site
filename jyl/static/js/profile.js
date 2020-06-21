b79b2d481551c78ba479467134b515f3ff2d3331
RafaelCenzano <savagecoder77@gmail.com>
2020-06-21 01:27:41 -0700

    indentation

--- jyl/static/js/profile.js
+++ jyl/static/js/profile.js
@@ -5,17 +5,17 @@ window.onload = function profileCircle() {
     var greenNum = 0;
     var blueNum = 0;
     if(number >= 85){
-      greenNum = 255;
+        greenNum = 255;
     }else if(number < 42.5){
         greenNum = 0;
-      redNum = 255;
-      greenNum += 6 * number;
+        redNum = 255;
+        greenNum += 6 * number;
     }else{
         redNum = 255;
-      greenNum = 255;
-      redNum -= 6 * (number - 42.5);
+        greenNum = 255;
+        redNum -= 6 * (number - 42.5);
     }
     for (var i = 0; i < values.length; i++) {
-      values[i].style.backgroundColor = "rgb(" + redNum + ", " + greenNum + ", 0)";
+        values[i].style.backgroundColor = "rgb(" + redNum + ", " + greenNum + ", 0)";
     }
 }
