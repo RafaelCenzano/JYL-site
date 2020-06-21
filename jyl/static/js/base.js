0c57694dc7987cc503c2b7ea2be797988324e59c
RafaelCenzano <savagecoder77@gmail.com>
2020-06-21 01:26:26 -0700

    move flash js to base js

--- jyl/static/js/base.js
+++ jyl/static/js/base.js
@@ -28,3 +28,14 @@ function jsTextArea(ta, span, number){
         }
     });
 }
+
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function(){
+    var div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function(){ div.style.display = "none"; }, 600);
+    }
+}
