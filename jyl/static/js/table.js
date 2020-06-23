da6ad15042d1b85cd2f358f71d04664630d7c42b
RafaelCenzano <savagecoder77@gmail.com>
2020-06-23 12:20:35 -0700

    remove js

--- jyl/static/js/table.js
+++ jyl/static/js/table.js
@@ -24,31 +24,3 @@ $(document).ready(function() {
     });
 
 } );
-
-function isDarkModeEnabled() {
-            const siteThemeFromStorage = window.localStorage.getItem("site-theme");
-            return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && (siteThemeFromStorage !== "light")) || siteThemeFromStorage === "dark";
-}
-let isDarkModePreferred = isDarkModeEnabled();
-
-let theme = isDarkModePreferred ? "dark" : "light"
-
-if (theme === "dark") {
-    var all = document.getElementsByTagName("*");
-    for (var i=0, max=all.length; i < max; i++) {
-        if (all[i] != null) {
-            var str = String(all[i].className)
-            if (str.substring(str.length - 5, str.length) == 'light'){
-                all[i].className = str.substring(0, str.length - 6);
-            }
-        }
-    }
-} else {
-    var all = document.getElementsByTagName("*");
-
-    for (var i=0, max=all.length; i < max; i++) {
-        if (str.substring(str.length - 5, str.length) != 'light'){
-            all[i].className += " light";
-        }
-    }
-}
