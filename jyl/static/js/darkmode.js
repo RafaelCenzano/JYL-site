66c26e22ec2ff912e9ab2643bf1c1f7ac92480ea
RafaelCenzano <savagecoder77@gmail.com>
2020-06-23 23:07:58 -0700

    attempt updates to navbar dark mode light mode switcher

--- jyl/static/js/darkmode.js
+++ jyl/static/js/darkmode.js
@@ -1,47 +1,63 @@
 function isDarkModeEnabled() {
-            const siteThemeFromStorage = window.localStorage.getItem("site-theme");
-            return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && (siteThemeFromStorage !== "light")) || siteThemeFromStorage === "dark";
+    const siteThemeFromStorage = window.localStorage.getItem("site-theme");
+    return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && (siteThemeFromStorage !== "light")) || siteThemeFromStorage === "dark";
 }
+
 let isDarkModePreferred = isDarkModeEnabled();
+
 function switchTheme(theme) {
-  if (theme === "dark") {
-    document.documentElement.setAttribute("site-theme", "dark");
-    window.localStorage.setItem("site-theme", "dark");
-    var all = document.getElementsByTagName("*");
-    for (var i=0, max=all.length; i < max; i++) {
-        if (all[i] != null) {
-            var str = String(all[i].className)
-            if (str.substring(str.length - 5, str.length) == 'light'){
-                all[i].className = str.substring(0, str.length - 6);
+    if (theme === "dark") {
+        document.documentElement.setAttribute("site-theme", "dark");
+        window.localStorage.setItem("site-theme", "dark");
+
+        var all = document.getElementsByTagName("*");
+        for (var i=0, max=all.length; i < max; i++) {
+            if (all[i] != null) {
+                var str = String(all[i].className)
+                if (str.substring(str.length - 5, str.length) == 'light'){
+                    all[i].className = str.substring(0, str.length - 6);
+                }
             }
         }
-    }
-  } else {
-    document.documentElement.setAttribute("site-theme", "light");
-    window.localStorage.setItem("site-theme", "light");
-    var all = document.getElementsByTagName("*");
+    } else {
+        document.documentElement.setAttribute("site-theme", "light");
+        window.localStorage.setItem("site-theme", "light");
 
-    for (var i=0, max=all.length; i < max; i++) {
-        all[i].className += " light";
+        var all = document.getElementsByTagName("*");
+        for (var i=0, max=all.length; i < max; i++) {
+            all[i].className += " light";
+        }
     }
-  }
 }
 
 function overrideTheme(button) {
-  if (!isDarkModePreferred) {
-    isDarkModePreferred = true;
-    this.innerHTML = "Switch to Light Theme";
-    switchTheme("dark");
-  } else {
-    isDarkModePreferred = false;
-    this.innerHTML = "Switch to Dark Theme";
-    switchTheme("light");
-  }
+    let item = document.getElementById("switchtext");
+    let icon = document.getElementById("icon");
+
+    if (!isDarkModePreferred) {
+        isDarkModePreferred = true;
+        item.innerHTML = "Light Theme";
+        icon.className = "far fa-moon";
+        switchTheme("dark");
+    } else {
+        isDarkModePreferred = false;
+        item.innerHTML = "Dark Theme";
+        icon.className = "fas fa-moon";
+        switchTheme("light");
+    }
 }
 
 switchTheme(isDarkModePreferred ? "dark" : "light");
 
 const themeButton = document.getElementById('theme-btn');
-themeButton.innerHTML = isDarkModePreferred ? "Switch to Light Theme" : "Switch to Dark Theme";
+let item = document.getElementById("switchtext");
+let icon = document.getElementById("icon");
+if (isDarkModePreferred) {
+    item.innerHTML = "Light Theme";
+    icon.className = "far fa-moon";
+} else {
+    item.innerHTML = "Dark Theme";
+    icon.className = "fas fa-moon";
+}
 themeButton.onclick = overrideTheme;
 overrideTheme.bind(themeButton);
