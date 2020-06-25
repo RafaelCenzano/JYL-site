baafa52f36275b74ea6d245e50f2a631ae1072ef
RafaelCenzano <savagecoder77@gmail.com>
2020-06-24 19:34:28 -0700

    update indentation

--- jyl/static/js/base.js
+++ jyl/static/js/base.js
@@ -46,7 +46,7 @@ var i;
 
 for (i = 0; i < close.length; i++) {
 close[i].onclick = function(){
-    var div = this.parentElement;
+        var div = this.parentElement;
         div.style.opacity = "0";
         setTimeout(function(){ div.style.display = "none"; }, 600);
     }
