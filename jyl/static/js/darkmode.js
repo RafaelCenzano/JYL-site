78a88c89a256a9404b1eda78992098b671f8880a
RafaelCenzano <savagecoder77@gmail.com>
2020-06-24 19:50:32 -0700

    update transitions with js

--- jyl/static/js/darkmode.js
+++ jyl/static/js/darkmode.js
@@ -15,6 +15,45 @@ function switchTheme(theme) {
             if (all[i] != null) {
                 var str = String(all[i].className)
                 if (str.substring(str.length - 5, str.length) == 'light'){
+                    all[i].style = "transition-duration: 0.6s";
+                    all[i].className = str.substring(0, str.length - 6);
+                }
+            }
+        }
+        setTimeout(function(){
+            var all = document.getElementsByTagName("*");
+            for (var i=0, max=all.length; i < max; i++) {
+                all[i].style = String(all[i].style).substring(0, str.length - 25);
+            }
+        }, 1500);
+    } else {
+        document.documentElement.setAttribute("site-theme", "light");
+        window.localStorage.setItem("site-theme", "light");
+
+        var all = document.getElementsByTagName("*");
+        for (var i=0, max=all.length; i < max; i++) {
+            all[i].style = "transition-duration: 0.6s";
+            all[i].className += " light";
+        }
+        setTimeout(function(){
+            var all = document.getElementsByTagName("*");
+            for (var i=0, max=all.length; i < max; i++) {
+                all[i].style = String(all[i].style).substring(0, str.length - 25);
+            }
+        }, 1500);
+    }
+}
+
+function switchThemeNormal(theme) {
+    if (theme === "dark") {
+        document.documentElement.setAttribute("site-theme", "dark");
+        window.localStorage.setItem("site-theme", "dark");
+
+        var all = document.getElementsByTagName("*");
+        for (var i=0, max=all.length; i < max; i++) {
+            if (all[i] != null) {
+                var str = String(all[i].className)
+                if (str.substring(str.length - 5, str.length) == 'light'){
                     all[i].className = str.substring(0, str.length - 6);
                 }
             }
@@ -47,7 +86,7 @@ function overrideTheme(button) {
     }
 }
 
-switchTheme(isDarkModePreferred ? "dark" : "light");
+switchThemeNormal(isDarkModePreferred ? "dark" : "light");
 
 const themeButton = document.getElementById('theme-btn');
 let item = document.getElementById("switchtext");
