bd4bb40046c65142b82f34e792beb8f6b86e5f3f
RafaelCenzano <savagecoder77@gmail.com>
2020-06-28 01:52:33 -0700

    revert

--- jyl/static/js/profile.js
+++ jyl/static/js/profile.js
@@ -1,6 +1,6 @@
 window.onload = function profileCircle() {
-    var values = document.getElementById('hours');
-    var number = parseInt(values.value);
+    var values = document.getElementsByClassName('hours');
+    var number = parseInt(values[0].textContent);
     var redNum = 0;
     var greenNum = 0;
     var blueNum = 0;
